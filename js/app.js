311e6d808d35b8fe26314e078db352f67129e7d2
Sang Lee <sanglee76@yahoo.com>
2020-12-02 07:08:24 -0800

    partially completed lab2

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 const hornedAnimalsArray = [];
 
-// TODO: define constructor
+// define constructor
 function HornedAnimal(jsonObject){
   this.image_url = jsonObject.image_url;
   this.title = jsonObject.title;
@@ -12,39 +12,48 @@ function HornedAnimal(jsonObject){
 }
 
 
-// TODO: define prototype render method
+// define prototype render method
 HornedAnimal.prototype.render = function(){
-  // TODO: Use jQuery to make a copy of the HTML template of the photo component.
+  // Use jQuery to make a copy of the HTML template of the photo component.
   const $newHornedAnimalDiv = $('#photo-template').clone();
   
-  // TODO: assign each property of the object instance with the json values 
+  // assign each property of the object instance with the json values 
   $newHornedAnimalDiv.find('h2').text(this.title);
   $newHornedAnimalDiv.find('img').attr('src', this.image_url);
+  $newHornedAnimalDiv.find('img').attr('alt', this.keyword);
   $newHornedAnimalDiv.find('p').text(this.description);
   $newHornedAnimalDiv.removeAttr('id')
   
-  // TODO: update the DOM with the new horned animal object
-  $('#hornsEverywhere').append($newHornedAnimalDiv);
-
+  // update the DOM with the new horned animal object
+  $('#container').append($newHornedAnimalDiv);
 }
 
-// TODO: build image filter using drop down menu
+// TODO: build image filter using drop down menu using an event listener and handler -- ** STUCK ON THIS **
+$('.animal-select').on('change', e => {
+  e.preventDefault();
+  console.log(e.target.value);
+  const selectedAnimal = e.target.value;
+  for (let i=0; i < hornedAnimalsArray.length; i++){
+    // console.log(hornedAnimalsArray[i].keyword);
+    if (selectedAnimal === hornedAnimalsArray[i].keyword){
+      console.log(hornedAnimalsArray[i].keyword);
+      //TODO: render only the animals from the array that have the matching keyword attribute 
 
-  // TODO: build a form with a drop down menu using the keywords from the json data
-
-  // TODO: create an event handler
-
-  // TODO: create an event listener
+    }
+  }
+});
 
 
-// TODO: use $.ajax to get json files 
-// TODO: Note to SELF(Ask a TA): what is the second paramater 'json' in reference to?
+
+
+// use $.ajax to get json files 
+// TODO: SELF(Ask a TA): what is the second paramater 'json' in reference to?
 $.ajax('https://codefellows.github.io/code-301-guide/curriculum/class-02/lab/starter-code/page-1.json', 'json').then(retrievedJSONAnimalData => {
 
-  // TODO: push each json object through the constructor function to build new HornedAnimal object instance
+  // push each json object through the constructor function to build new HornedAnimal object instance
   retrievedJSONAnimalData.forEach(hornedAnimalJSONObject => hornedAnimalsArray.push(new HornedAnimal(hornedAnimalJSONObject)));
 }).then(() => {
-  // TODO: call the render function to render the image 
+  // call the render function to render the image 
   hornedAnimalsArray.forEach(hornedAnimal => hornedAnimal.render());
 
 })
